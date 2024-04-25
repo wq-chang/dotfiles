e943f09dcd509de6fe884d5ce656b3b3b644491e
WQ <wingquan.chang@gmail.com>
2024-04-26 02:26:10 +0800

    neovim: update hl config

--- configs/neovim/.config/nvim/lua/plugins/theme.lua
+++ configs/neovim/.config/nvim/lua/plugins/theme.lua
@@ -15,19 +15,19 @@ return {
 			local hl = function(name, val)
 				vim.api.nvim_set_hl(0, name, val)
 			end
-			hl("BqfPreviewTitle", { fg = c.border_highlight, bg = c.bg_float })
+			hl("BqfPreviewTitle", { link = "FloatBorder" })
 
 			hl("DapUIScope", { fg = c.border_highlight })
 			hl("DapUIModifiedValue", { fg = c.border_highlight })
 			hl("DapUIDecoration", { fg = c.border_highlight })
 			hl("DapUIStoppedThread", { fg = c.border_highlight })
 			hl("DapUILineNumber", { fg = c.border_highlight })
-			hl("DapUIFloatBorder", { fg = c.border_highlight, bg = c.bg_float })
+			hl("DapUIFloatBorder", { link = "FloatBorder" })
 			hl("DapUIBreakpointsPath", { fg = c.border_highlight })
-			hl("DapUIStepInto", { fg = c.border_highlight, bg = c.bg_float })
-			hl("DapUIStepOver", { fg = c.border_highlight, bg = c.bg_float })
-			hl("DapUIStepBack", { fg = c.border_highlight, bg = c.bg_float })
-			hl("DapUIStepOut", { fg = c.border_highlight, bg = c.bg_float })
+			hl("DapUIStepInto", { fg = c.border_highlight, bg = c.bg_dark })
+			hl("DapUIStepOver", { fg = c.border_highlight, bg = c.bg_dark })
+			hl("DapUIStepBack", { fg = c.border_highlight, bg = c.bg_dark })
+			hl("DapUIStepOut", { fg = c.border_highlight, bg = c.bg_dark })
 			hl("DapUIWinSelect", { ctermfg = "Cyan", fg = c.border_highlight })
 
 			hl("DapUIType", { fg = c.purple })
@@ -37,15 +37,15 @@ return {
 			hl("DapUIWatchesValue", { fg = c.teal })
 			hl("DapUIBreakpointsInfo", { fg = c.teal })
 			hl("DapUIBreakpointsCurrentLine", { fg = c.teal })
-			hl("DapUIPlayPause", { fg = c.teal, bg = c.bg_float })
-			hl("DapUIRestart", { fg = c.teal, bg = c.bg_float })
+			hl("DapUIPlayPause", { fg = c.teal, bg = c.bg_dark })
+			hl("DapUIRestart", { fg = c.teal, bg = c.bg_dark })
 
 			hl("DapUIWatchesEmpty", { fg = c.red1 })
 			hl("DapUIWatchesError", { fg = c.red1 })
-			hl("DapUIStop", { fg = c.red1, bg = c.bg_float })
+			hl("DapUIStop", { fg = c.red1, bg = c.bg_dark })
 
-			hl("DapUIBreakpointsDisabledLine", { fg = "#424242" })
-			hl("DapUIUnavailable", { fg = "#424242" })
+			hl("DapUIBreakpointsDisabledLine", { fg = c.fg_gutter })
+			hl("DapUIUnavailable", { fg = c.fg_gutter, bg = c.bg_dark })
 			vim.cmd([[colorscheme tokyonight]])
 		end,
 	},
