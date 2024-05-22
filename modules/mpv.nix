fb332c539ddd6faef2de1832674cf20613f97c31
WQ <wingquan.chang@gmail.com>
2024-05-23 01:05:35 +0800

    use native mpv conf

--- modules/mpv.nix
+++ modules/mpv.nix
@@ -1,39 +1,25 @@
-{ deps, pkgs, ... }:
+{
+  config,
+  deps,
+  pkgs,
+  ...
+}:
 with pkgs;
 let
-  icon = {
-    clock = "  ";
-    checked = "  ";
-  };
   simpleHistory = callPackage ../packages/mpv-scripts-simple-history.nix { inherit deps; };
 in
 {
-
   programs.mpv = {
     enable = true;
-    config = {
-      osd-bar = "no";
-      gpu-api = "vulkan";
-    };
     scripts = with mpvScripts; [
       autoload
       uosc
       simpleHistory
       thumbfast
     ];
-    scriptOpts = {
-      SimpleHistory = {
-        auto_run_list_idle = "distinct";
-        resume_option = "force";
-        resume_option_threshold = 0;
-
-        time_seperator = icon.clock;
-        header_list_duration_pre_text = icon.clock;
-        header_list_length_pre_text = icon.clock;
-        header_list_remaining_pre_text = icon.clock;
-        text_highlight_pre_text = icon.checked;
-        header_highlight_pre_text = icon.checked;
-      };
-    };
   };
+
+  xdg.configFile.mpv.source =
+    with config;
+    lib.file.mkOutOfStoreSymlink "${home.homeDirectory}/dotfiles/config/mpv";
 }
