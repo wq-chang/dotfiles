92e76173a8f5a87aa2bf0f9d5befb57407395007
WQ <wingquan.chang@gmail.com>
2024-05-29 02:26:41 +0800

    add noto fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,10 +1,38 @@
 { pkgs, ... }:
 with pkgs;
 {
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      emoji = [
+        "Symbols Nerd Font"
+        "Noto Color Emoji"
+      ];
+      monospace = [
+        "Noto Sans Mono"
+        "Monospace"
+      ];
+      sansSerif = [
+        "Noto Sans CJK SC"
+        "Noto Sans CJK TC"
+        "Noto Sans CJK JP"
+        "Noto Sans"
+      ];
+      serif = [
+        "Noto Serif CJK SC"
+        "Noto Serif CJK TC"
+        "Noto Serif CJK JP"
+        "Noto Serif"
+      ];
+    };
+  };
+
   home.packages = [
-    corefonts
     jetbrains-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
     (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
   ];
 }
