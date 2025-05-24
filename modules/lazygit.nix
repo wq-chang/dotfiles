bfa7b899fd12990ca15dfc2545694145a269ede0
WQ <wingquan.chang@gmail.com>
2025-05-24 10:05:42 +0000

    use yml to config lazygit and update deps

--- modules/lazygit.nix
+++ modules/lazygit.nix
@@ -10,32 +10,9 @@ let
   homeConfig = {
     programs.lazygit = {
       enable = true;
-      settings = {
-        os.editPreset = "nvim";
-        git = {
-          disableForcePushing = true;
-          paging = {
-            colorArg = "always";
-            pager = "delta --line-numbers --paging=never";
-          };
-        };
-        gui.theme = {
-          activeBorderColor = [
-            "#7aa2f7"
-            "bold"
-          ];
-          inactiveBorderColor = [ "#545c7e" ];
-          optionsTextColor = [ "#7aa2f7" ];
-          selectedLineBgColor = [ "#292e42" ];
-          selectedRangeBgColor = [ "#283457" ];
-          cherryPickedCommitBgColor = [ "#283457" ];
-          cherryPickedCommitFgColor = [ "#c0caf5" ];
-          unstagedChangesColor = [ "#f7768e" ];
-          defaultFgColor = [ "#c0caf5" ];
-          searchingActiveBorderColor = [ "#27a1b9" ];
-        };
-      };
     };
+
+    xdg.configFile.lazygit.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/lazygit";
   };
 in
 {
