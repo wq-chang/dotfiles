85eea9374ef123c95b1069de99132cbade5dbc79
WQ <wingquan.chang@gmail.com>
2025-10-04 18:02:32 +0000

    update deps

--- packages/aws-local.nix
+++ packages/aws-local.nix
@@ -2,13 +2,13 @@
   pkgs,
 }:
 pkgs.python3Packages.buildPythonApplication rec {
-  pname = "awscli-local";
-  version = "0.22.0";
+  pname = "awscli_local";
+  version = "0.22.2";
   pyproject = true;
 
   src = pkgs.fetchPypi {
     inherit pname version;
-    hash = "sha256-OAfPLuS73U3038i+8CfyW95SPcr4EZcg9nftleu6ZqQ=";
+    hash = "sha256-B8Uyw3J1O/XxVCZFHckdbuyd6HeXSASTKamogr2sigs=";
   };
 
   build-system = with pkgs.python3Packages; [ setuptools ];
