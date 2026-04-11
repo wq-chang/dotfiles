70de97700bbdaac32b55d47b1be3109b3637683c
WQ <wingquan.chang@gmail.com>
2026-04-11 10:29:40 +0000

    fix codecompanion config and add keymaps

--- config/neovim/lua/plugins/codecompanion.lua
+++ config/neovim/lua/plugins/codecompanion.lua
@@ -5,6 +5,14 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
+	keys = {
+		-- stylua: ignore start
+		{ "<leader>aa", "<cmd>CodeCompanionActions<cr>", mode = { "n", "x" }, desc = "Code Companion actions" },
+		{ "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", desc = "Toggle Code Companion chat" },
+		{ "<leader>aC", "<cmd>CodeCompanionChat<cr>", desc = "Create Code Companion chat" },
+		{ "<leader>ai", "<cmd>CodeCompanionInline<cr>", mode = "x", desc = "Code Companion inline" },
+		-- stylua: ignore end
+	},
 	opts = {
 		adapters = {
 			acp = {
@@ -24,17 +32,17 @@ return {
 			action_palette = {
 				provider = "fzf_lua",
 			},
-			interactions = {
-				chat = {
-					adapter = {
-						name = "copilot_acp",
-						model = "gpt-5-mini",
-					},
+		},
+		interactions = {
+			chat = {
+				adapter = {
+					name = "copilot_acp",
+					model = "gpt-5-mini",
 				},
-				inline = {
-					adapter = {
-						name = "copilot",
-					},
+			},
+			inline = {
+				adapter = {
+					name = "copilot",
 				},
 			},
 		},
--- config/neovim/lua/plugins/which-key.lua
+++ config/neovim/lua/plugins/which-key.lua
@@ -11,6 +11,7 @@ return {
 			{ "z", group = "+fold" },
 			{ "]", group = "+next" },
 			{ "[", group = "+prev" },
+			{ "<leader>a", group = "+ai" },
 			{ "<leader>c", group = "+code" },
 			{ "<leader>d", group = "+debug" },
 			{ "<leader>f", group = "+file/find" },
